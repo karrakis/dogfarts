1b134c6194f81da20df17d0f24ee3dda5f971a2f
Rachel Broadbear <rbroadbear@gmail.com>
2016-08-22 22:32:34 -0500

    Updating before I take my computer for repairs

--- Rachel/fartbot/RachelsFartBot.rb
+++ Rachel/fartbot/RachelsFartBot.rb
@@ -1,10 +1,15 @@
-require "engtagger"
+require 'engtagger'
+require 'mysql2'
 
 class RachelsFartBot
 
 	attr_accessor :subjects
 
 	def initialize
+
+		@client = Mysql2::Client.new(:host => "localhost", :database=>"rachels_dog_farts", :password=>"stinkydogfarts", :username => 'root')
+		#define the number of times a subject can be used as the input
+		@MAXUSES = 4
 
 	end
 
@@ -108,7 +113,80 @@ class RachelsFartBot
 		
 	end
 
-	def err_handler()
+	def err_handler(garbled)
+
+		#if err_handler is called, select a random sentence to output
+
+		output = @client.query("SELECT random_sentence FROM random_sentence ORDER BY RAND() LIMIT 1;").to_a.first["random_sentence"]
+		#puts(output)
+
 	end
 
+	def database_update_inputs(sentence, subject)
+
+		#enters input sentence and subject into the database		
+
+		@client.query("INSERT INTO input_sentences (sentence, num_uses) VALUES ('#{sentence}','1') ON DUPLICATE KEY UPDATE num_uses=num_uses+1")
+		@client.query("INSERT INTO input_subjects (subject, num_uses) VALUES ('#{subject}','1') ON DUPLICATE KEY UPDATE num_uses=num_uses+1")
+		
+	end
+
+	def database_update_outputs(output, subject)
+
+		#enters output sentence and subject into the database	
+
+		@client.query("INSERT INTO output_sentences (sentence, num_uses) VALUES ('#{output}','1') ON DUPLICATE KEY UPDATE num_uses=num_uses+1")
+		@client.query("INSERT INTO output_subjects (subject, num_uses) VALUES ('#{subject}','1') ON DUPLICATE KEY UPDATE num_uses=num_uses+1")
+
+	end
+
+	def check_input_redundancy(sentence,subject)
+
+		#checks input sentence and subject to see if they've been used before
+		#if input sentence is a repeat, answer as before but angry
+		#if subject is getting old, answer the question and then change the subject
+
+		subject_repeat = @client.query("SELECT num_uses FROM input_subjects WHERE subject='#{subject}'").to_a.first["num_uses"]
+		sentence_repeat = @client.query("SELECT num_uses FROM input_sentences WHERE sentence='#{sentence}'").to_a.first["num_uses"]
+		id = @client.query("SELECT id FROM input_sentences WHERE sentence='#{sentence}'").first["id"]
+		if sentence_repeat > 1
+			get_angry(id,sentence,sentence_repeat)
+			add_something_maybe(sentence,subject)
+		elsif subject_repeat > @MAXUSES
+			answer_question(sentence,subject)
+			change_subject
+		else
+			answer_question(sentence,subject)
+			add_something_maybe(sentence,subject)
+		end
+	end
+
+	def get_angry(id,sentence,num_uses)
+
+		#figure out what I said last time this was asked and make the first character lowercase
+		query = "SELECT sentence FROM output_sentences WHERE created_at >= (SELECT created_at FROM input_sentences WHERE id ='#{id}') ORDER BY created_at asc limit 1;"
+		previous_response = @client.query(query).first['sentence']
+		previous_response[0] = previous_response[0].downcase
+		
+		#responses depending on the number of times it's been asked
+
+		second_response = ["I already told you: " + previous_response, "Like I said before, " + previous_response, "Just like last time you asked, " + previous_response]
+		n_response = ["Dude, I've already answered this " + (num_uses - 1).to_s + " times before, but " + previous_response, "This question is getting really old.  I said: " + previous_response, "Do you actually have a memory? I told you " + previous_response]
+		too_many_response = ["I already told you.","Most cats are smarter than you are.  Stop asking me that.", "I'm done."]
+
+		#choose a response
+
+		if num_uses == 2
+			answer = second_response.sample
+		elsif (num_uses > 2) && (num_uses < 5)
+			answer = n_response.sample
+		elsif num_uses > 4
+			answer = too_many_response.sample
+		else
+			err_handler(sentence)
+		end
+
+		return answer
+
+	end
 end
