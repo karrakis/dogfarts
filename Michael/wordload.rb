64a285990f3efa38874c597728945b2ff86b91c1
michael <moelkers2010@gmail.com>
2016-08-28 21:39:48 -0500

    worldload.rb loads database of words into single string.

--- Michael/wordload.rb
+++ Michael/wordload.rb
@@ -6,5 +6,12 @@ require 'mysql2'
 
 def loadLibArray()
 	client = Mysql2::Client.new(:host=>"localhost", :database=>"chatbot", :password=>12345, :username=>'root')
-	client.query("SELECT Word FROM wordbank")
+	queryResult = client.query("SELECT Word FROM wordbank")
+	queryResultArray = queryResult.to_a
+	target = Array.new()
+	for i in queryResultArray
+		target << i["Word"]
+	end
+	return target.join(" ")
+
 end
